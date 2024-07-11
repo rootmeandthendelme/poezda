ff9482c05023679f32def7d16d44a0f169155308
dont-you <mihailwhoss@gmail.com>
2024-07-11 13:12:06 +0300

    init

---
+++ src/main/java/ru/tututu/trains/TrainsApplication.java
@@ -0,0 +1,13 @@
+package ru.tututu.trains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tututu/trains/TrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tututu.trains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
