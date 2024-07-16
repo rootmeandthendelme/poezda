18d297bab1ca915ca0a5c261d9146a0e2a2064a7
rootmeandthendelme <mihail.travka@yandex.ru>
2024-07-16 14:19:52 +0300

    feat: add controllers

--- src/main/java/ru/tututu/trains/controller/ReservationController.java
+++ src/main/java/ru/tututu/trains/controller/ReservationController.java
@@ -1,7 +1,48 @@
 package ru.tututu.trains.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.tututu.trains.entity.Reservation;
+import ru.tututu.trains.model.ReservationRequestModel;
+import ru.tututu.trains.security.UserPrincipal;
+import ru.tututu.trains.service.ReservationService;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ReservationController {
+    @Autowired
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping("/reservations")
+    public @ResponseBody List<Reservation> reserve(@RequestBody @Validated List<ReservationRequestModel> reservations) {
+        UserPrincipal userPrincipal = (UserPrincipal)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        int userId = Math.toIntExact(userPrincipal.getUserId());
+        List<Reservation> response = new ArrayList<>();
+
+        for(ReservationRequestModel reservationRequestModel: reservations){
+            try {
+                response.add(reservationService.createReservation(reservationRequestModel, userId));
+            } catch (SQLException e) {
+                throw new ResponseStatusException(HttpStatusCode.valueOf(400), "sql error with msg - " + e.getMessage());
+            }
+        }
+
+        return response;
+    }
+
+    @DeleteMapping("/reservations/{id}")
+    public void reserve(@PathVariable int id) throws SQLException {
+        reservationService.deleteReservation(id);
+    }
 }
--- src/main/java/ru/tututu/trains/controller/TripController.java
+++ src/main/java/ru/tututu/trains/controller/TripController.java
@@ -1,7 +1,33 @@
 package ru.tututu.trains.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.tututu.trains.model.schedule.TripInformationResponse;
+import ru.tututu.trains.service.TripService;
 
-@RestController
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController()
 public class TripController {
+
+    @Autowired
+    private final TripService tripService;
+
+    public TripController(TripService tripService) {
+        this.tripService = tripService;
+    }
+
+    @GetMapping("/trips/search")
+    public List<TripInformationResponse> getAvailableTrips(
+            @RequestParam("date") Optional<String> date,
+            @RequestParam("trainName") Optional<String> trainName,
+            @RequestParam("startPoint") String startPoint,
+            @RequestParam("endPoint") String endPoint
+    ) throws SQLException {
+        return tripService.getTripsSchedule(date, trainName, startPoint, endPoint);
+    }
 }
